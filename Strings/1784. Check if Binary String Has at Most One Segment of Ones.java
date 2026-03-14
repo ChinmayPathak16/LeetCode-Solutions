2ea22bff354a8ad2b6fb18ea6506a79635a8f59f
Chinmay Pathak <chinmaypathak1608@gmail.com>
2026-03-14 23:29:42 +0530

    Implement checkOnesSegment method for binary strings

---
+++ Strings/1784. Check if Binary String Has at Most One Segment of Ones.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean checkOnesSegment(String s) {
+        int n = s.length();
+        int i = 0;
+        while(i < n && s.charAt(i) == '0'){
+            i++;
+        }
+        while(i < n && s.charAt(i) != '0'){
+            i++;
+        }
+        while(i < n){
+            if(s.charAt(i) == '1'){
+                return false;
+            }
+            i++;
+        }
+        return true;
+    }
+}
