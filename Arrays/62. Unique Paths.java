09d3c2af97dd2a89629f73753a9c75d64da5d045
Chinmay Pathak <chinmaypathak1608@gmail.com>
2026-03-20 00:22:07 +0530

    Implement unique paths solution using dynamic programming

---
+++ Arrays/62. Unique Paths.java
@@ -0,0 +1,23 @@
+class Solution {
+    int[][] dp;
+
+    int f(int i, int j) {
+        if (i == 0 || j == 0) return 1;
+        if (i < 0 || j < 0) return 0;
+
+        if (dp[i][j] != -1) return dp[i][j];
+
+        int up = f(i - 1, j);
+        int left = f(i, j - 1);
+
+        return dp[i][j] = up + left;
+    }
+
+    public int uniquePaths(int m, int n) {
+        dp = new int[m][n];
+        for (int[] row : dp) {
+            Arrays.fill(row, -1);
+        }
+        return f(m - 1, n - 1);
+    }
+}
