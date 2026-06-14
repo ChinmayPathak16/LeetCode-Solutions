f3bf89d387947d77d79f28846c5b5b0a6a348f94
Chinmay Pathak <chinmaypathak1608@gmail.com>
2026-06-14 18:00:32 +0530

    Create 3838. Weighted Word Mapping.java

---
+++ Arrays/3838. Weighted Word Mapping.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String mapWordWeights(String[] words, int[] weights) {
+        StringBuilder s = new StringBuilder();
+
+        for (int i = 0; i < words.length; i++) {
+            int sum = 0;
+
+            for (int j = 0; j < words[i].length(); j++) {
+                char ch = words[i].charAt(j);
+                sum += weights[ch - 'a'];
+            }
+
+            sum %= 26;
+            s.append((char)('z'-sum));
+        }
+
+        return s.toString();
+    }
+}
