712591587fd785c2eb3058f9aa7fd96c94b3a372
Chinmay Pathak <chinmaypathak1608@gmail.com>
2026-09-25 23:10:07 +0530

    Implement flip and invert image function

---
+++ Arrays/832. Flipping an Image.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[][] flipAndInvertImage(int[][] image) {
+
+        int n = image.length;
+        int[][] res = new int[n][n];
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                res[i][n-1-j] = (image[i][j] + 1)%2;
+            }
+        }
+        return res;
+    }
+}
